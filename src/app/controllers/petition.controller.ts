c5be41b033dc3a63f27661bfbdc7134c24f21f4a
Cindy Bond <cbo680@cs19119bs.canterbury.ac.nz>
2024-03-25 09:38:38 +1300

    Petition first 3 tests passed

--- src/app/controllers/petition.controller.ts
+++ src/app/controllers/petition.controller.ts
@@ -1,13 +1,25 @@
 import {Request, Response} from "express";
 import Logger from '../../config/logger';
+import * as petition from '../models/petition.model';
 
 const getAllPetitions = async (req: Request, res: Response): Promise<void> => {
     Logger.info('GET all petitions')
-    try{
-        // Your code goes here
+    let startIndex = parseInt(req.query.startIndex as string,10);
+    const count = parseInt(req.query.count as string,10);
+    const q = (req.query.q as string);
+    const categoryIds = req.query.categoryIds;
+    const supportingCost = req.query.supportingCost;
+    const ownerId = req.query.ownerId;
+    const supporterId = req.query.supporterId;
+    let endIndex = startIndex + count;
 
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+    try{
+        const result = await petition.viewAllPetitions(q);
+        if (Number.isNaN(startIndex)) {
+            startIndex = 0;
+            endIndex = result.length;
+        }
+        res.status(200).send({"petitions": result.slice(startIndex,endIndex), "count": result.length});
         return;
     } catch (err) {
         Logger.error(err);
